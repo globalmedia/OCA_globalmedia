8efa42180068d617b3e48514d5ce6fef4a3bc4d8
jozephbrasil <jozephbrasil@users.noreply.github.com>
2025-11-09 08:06:11 -0300

    Update InfinitePay integration details in manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,13 +1,17 @@
 {
-    'name': 'Pagamento InfinitePay',
-    'version': '1.0.6',
-    'category': 'Website',
-    'summary': 'Integração com InfinitePay Checkout',
+    'name': 'Payment InfinitePay (acquirer)',
+    'version': '1.0.7',
+    'category': 'Accounting/Payment',
+    'summary': 'InfinitePay Checkout integration as payment acquirer',
     'author': 'GlobalMedia',
-    'depends': ['website_sale'],
+    'depends': ['payment', 'website_sale', 'sale'],
     'data': [
-        'data/ir_cron.xml',
+        'data/payment_infinitepay_data.xml',
+        'views/payment_infinitepay_views.xml',
+        'views/payment_infinitepay_templates.xml',
+        'views/payment_infinitepay_assets.xml',
     ],
     'installable': True,
     'application': False,
+    'license': 'Apache 2.0',
 }

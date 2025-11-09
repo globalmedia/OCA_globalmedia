5c8c0e10a6139e33bec7026ee6877c051d176218
jozephbrasil <jozephbrasil@users.noreply.github.com>
2025-11-09 07:14:35 -0300

    Bump version to 1.0.1 in __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Pagamento InfinitePay',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'category': 'Website',
     'summary': 'Integração com InfinitePay Checkout',
     'author': 'GlobalMedia',

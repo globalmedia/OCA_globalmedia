00f2e718c4eec6db51766b41dc2f9aea7cc898eb
jozephbrasil <jozephbrasil@users.noreply.github.com>
2025-11-09 07:40:28 -0300

    Refactor ResConfigSettings to add config parameter
    Updated infinitepay_handle field to include config parameter.

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -3,15 +3,7 @@ from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    infinitepay_handle = fields.Char(string="InfinitePay Handle")
-
-    def set_values(self):
-        super().set_values()
-        self.env['ir.config_parameter'].sudo().set_param('infinitepay.handle', self.infinitepay_handle)
-
-    def get_values(self):
-        res = super().get_values()
-        res.update({
-            'infinitepay_handle': self.env['ir.config_parameter'].sudo().get_param('infinitepay.handle'),
-        })
-        return res
+    infinitepay_handle = fields.Char(
+        string="InfinitePay Handle",
+        config_parameter='payment_infinitepay.infinitepay_handle',
+    )

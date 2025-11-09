5fd740c5643c22874c327681b9ef08f876830201
jozephbrasil <jozephbrasil@users.noreply.github.com>
2025-11-09 08:10:53 -0300

    Delete models/res_config_settings.py

--- models/res_config_settings.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    infinitepay_handle = fields.Char(
-        string="InfinitePay Handle",
-        config_parameter='payment_infinitepay.infinitepay_handle',
-    )
